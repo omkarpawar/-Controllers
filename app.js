dbb9294e93417d26e3cec4c38dfbb3802aebb694
omkarpawar <omkar111pawar@gmail.com>
2024-03-07 20:05:41 +0530

    first commit

--- app.js
+++ app.js
@@ -2,8 +2,33 @@ const http = require('http');
 
 const server = http.createServer((req,res)=>{
 
-    res.writeHead(200,{'Content-Type':'text/plain'});
-    res.end('my Name is Omkar');
+    res.writeHead(200,{'Content-Type':'text/html'});
+    res.end("welcome to the page");
+
+
+    const url=req.url;
+
+    if(url==='/home'){
+
+        res.writeHead=(200,{'Content-Type':'text/html'});
+        res.end('Welcome Home ');
+
+    }else if(url==='/about'){
+
+        res.writeHead=(200,{'Content-Type':'text/html'});
+        res.end('Welcome to about');
+
+    }else if(url === '/node'){
+
+        res.writeHead=(200,{'Content-Type':'text/html'});
+        res.end('Welcome to Node');
+
+    }else{
+
+        res.writeHead=(200,{'Content-Type':'text/html'});
+        res.end('404 NOT FOUND');
+    }
+
 });
 
 const PORT=4000;

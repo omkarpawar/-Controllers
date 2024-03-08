56eb5c1bad40ad18d836294e4af25ff33ed2171a
omkarpawar <omkar111pawar@gmail.com>
2024-03-08 17:20:35 +0530

    first commit

--- app.js
+++ app.js
@@ -1,55 +1,8 @@
 const http =require('http');
-const fs = require('fs');
 
-const server = http.createServer((req,res)=>{
-    const url=req.url;
-    const method = req.method;
-    if(url === '/'){
-        fs.readFile("message1.txt",{encoding:"utf-8"},(err,data)=>{
-            if(err){
-                console.log(err);
-            }
-            
-            res.write('<html>');
-            res.write('<head><title>demo</title></head>');
-            res.write(`<body>${data}</body>`);
-            res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit"  >SUBMIT</button></form></body>')
-            res.write('</html>')
-            return res.end();
-        })
-       
+const routes=require('./routes');
 
-
-    }
-    if(url === '/message' && method ==='POST' ){
-
-        const body =[];
-        req.on('data',(chunk)=>{
-            console.log(chunk);
-            body.push(chunk);
-        });
-
-        req.on('end',()=>{
-            const parseBody=Buffer.concat(body).toString();
-            const message=parseBody.split('=')[1];
-            fs.writeFile('message1.txt' , message, err =>{
-                if(err){
-                    console.log(err);
-                }
-                res.statusCode=302;
-                res.setHeader('Location','/');
-                return res.end();
-            });
-
-        });
-        
-        
-    }
-
-    res.setHeader('Content-Type','text/html');
-    
-
-})
+const server = http.createServer(routes);
 
 server.listen(4000);
     
---
+++ routes.js
@@ -0,0 +1,53 @@
+const fs =require('fs');
+
+const requestHandler = (req,res)=>{
+    const url = req.url;
+    const method=req.method;
+    if(url === '/'){
+        fs.readFile("message1.txt",{encoding:"utf-8"},(err,data)=>{
+            if(err){
+                console.log(err);
+            }
+            
+            res.write('<html>');
+            res.write('<head><title>demo</title></head>');
+            res.write(`<body>${data}</body>`);
+            res.write('<body><form action="/message" method="POST"><input type="text" name="message"></input><button type="submit"  >SUBMIT</button></form></body>')
+            res.write('</html>')
+            return res.end();
+        })
+       
+    
+    
+    }
+    if(url === '/message' && method ==='POST' ){
+    
+        const body =[];
+        req.on('data',(chunk)=>{
+            console.log(chunk);
+            body.push(chunk);
+        });
+    
+        req.on('end',()=>{
+            const parseBody=Buffer.concat(body).toString();
+            const message=parseBody.split('=')[1];
+            fs.writeFile('message1.txt' , message, err =>{
+                if(err){
+                    console.log(err);
+                }
+                res.statusCode=302;
+                res.setHeader('Location','/');
+                return res.end();
+            });
+    
+        });
+        
+        
+    }
+    
+    res.setHeader('Content-Type','text/html');
+    
+}
+
+
+module.exports=requestHandler;

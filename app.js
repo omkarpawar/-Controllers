71bb6af82f3ac8cfa6bad48a140fe3cbfe691c05
omkarpawar <omkar111pawar@gmail.com>
2024-03-09 13:02:49 +0530

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+
+const express = require('express');
 const http =require('http');
+const app = express();
 
-const routes=require('./routes');
+app.use((req, res, next)=>{
+    console.log("IN the middileware");
 
-const server = http.createServer(routes);
+    next();
+});
+app.use((req, res, next)=>{
+    console.log("In the another middleware");
+    res.send('<h1>HELLO FROM EXPRESS</h1>')
+});
 
-server.listen(4000);
+app.listen(3000);
     
 

1c6de0feefc0a921d8df8eb7411041fba0319c21
omkarpawar <omkar111pawar@gmail.com>
2024-03-09 14:57:15 +0530

    first commit

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 
 const express = require('express');
 const http =require('http');
+const bodyParser=require('body-parser');
 const app = express();
 
-app.use((req, res, next)=>{
-    console.log("IN the middileware");
+app.use(bodyParser.urlencoded());
 
-    next();
+app.use('/ap',(req, res, next)=>{
+    console.log("another page");
+    res.send('<form action ="/product" method="POST"><input type="text" placeholder="Product" name="title"><input type="text" placeholder="quantity" name="title"><button type="submit">ADD Product</button></form>')
 });
-app.use((req, res, next)=>{
-    console.log("In the another middleware");
-    res.send('<h1>HELLO FROM EXPRESS</h1>')
+
+app.post('/product',(req, res, next)=>{
+    console.log(req.body);
+    res.send('<h1>this is YOur Cart</h1>')
+    res.redirect('/');
 });
+
+
+app.use('/',(req, res, next)=>{
+    console.log("main page");
+    res.send('<h1>this is main page</h1>')
+});
+
+
 
 app.listen(3000);
     
